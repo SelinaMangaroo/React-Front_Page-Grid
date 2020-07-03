658c977d61587745b43da852e8e56722bcb9a02b
SelinaMangaroo <selinamangaroo@gmail.com>
2020-07-02 22:53:32 -0400

    updates

--- src/components/GridItem.js
+++ src/components/GridItem.js
@@ -1,37 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 
-import Card from "react-bootstrap/Card";
 import "bootstrap/dist/css/bootstrap.css";
+import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
 import "../App.css";
 
-function GridItem({ gridItem }) {
-  return (
-    <div className="col mb-4">
-      <div className="card-container">
-        <Card>
-          <div
-            className="img"
-            dangerouslySetInnerHTML={{ __html: gridItem.media_tag }}
-          />
-          <Card.Body>
-            <Card.Text>
-              <div
-                className="caption"
-                dangerouslySetInnerHTML={{ __html: gridItem.caption }}
-              />
-            </Card.Text>
-            <Button variant="outline-primary" size="sm">
-              <a className="url" href={gridItem.url} rel="noopener">
-                URL
-              </a>
-            </Button>
-          </Card.Body>
-        </Card>
+class GridItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="col mb-4">
+        <div className="card-container">
+          <Card>
+            <div
+              className="img"
+              dangerouslySetInnerHTML={{
+                __html: this.props.gridItem.media_tag,
+              }}
+            />
+            <Card.Body>
+              <Card.Text>
+                <div
+                  className="caption"
+                  dangerouslySetInnerHTML={{
+                    __html: this.props.gridItem.caption,
+                  }}
+                />
+              </Card.Text>
+              <Button variant="outline-primary" size="sm">
+                <a
+                  className="url"
+                  href={this.props.gridItem.url}
+                  rel="noopener"
+                >
+                  URL
+                </a>
+              </Button>
+            </Card.Body>
+          </Card>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default GridItem;
--- src/components/GridList.js
+++ src/components/GridList.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import GridItem from "./GridItem";
 
-function GridList(props) {
-  return (
-    <div>
-      <br></br>
+class GridList extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-      <div className="container-fluid">
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-3">
-          {props.gridItems.map((item) => {
-            return <GridItem key={item.key} gridItem={item} />;
-          })}
+  render() {
+    return (
+      <div>
+        <div className="container-fluid">
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-3">
+            {this.props.gridItems.map((item) => {
+              return <GridItem key={item.key} gridItem={item} />;
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default GridList;
